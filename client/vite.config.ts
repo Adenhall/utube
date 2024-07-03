0fbd5602e304e828d06fbc7de7b78508500ea0c3
Adenhall <jamesadenhall@gmail.com>
2024-07-03 22:05:29 +0700

    feat: render client on production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 4000
-  },
   plugins: [react()],
-})
+  build: {
+    outDir: "../public",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+      },
+    },
+  },
+  server: {
+    host: "localhost",
+    port: 3001,
+    proxy: {
+      "^/api/.*": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
