d3fe124eaa9f9633b16bffd0b5488785a767d963
Adenhall <jamesadenhall@gmail.com>
2024-07-06 03:16:57 +0700

    feat: remove app wide gray background

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -7,7 +7,7 @@ import AuthProvider from "../contexts/auth/AuthProvider";
 const Root = () => {
   return (
     <AuthProvider>
-      <div className="flex flex-col w-full max-h-screen bg-gray-100 overflow-hidden">
+      <div className="flex flex-col w-full max-h-screen overflow-hidden">
         <Header />
         <Outlet />
       </div>
