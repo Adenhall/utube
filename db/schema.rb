053d2c74ef072d441c522feea0abe7c33c01134b
Adenhall <jamesadenhall@gmail.com>
2024-07-06 00:00:50 +0700

    fix: fix database schema wrong user id and missing video title

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_05_153956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "videos", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "link"
+    t.uuid "user_id", null: false
+    t.string "link", null: false
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_videos_on_user_id"
