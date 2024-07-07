a7815fdacf327c09b6fb04829d81cc64106f5d4f
Adenhall <jamesadenhall@gmail.com>
2024-07-07 14:12:42 +0700

    test: add notifications channel tests

--- test/channels/notitifcations_channel_test.rb
+++ test/channels/notitifcations_channel_test.rb
@@ -1,8 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
-class NotitifcationsChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+class NotificationsChannelTest < ActionCable::Channel::TestCase
+  test 'subscribes to stream' do
+    subscribe
+
+    assert subscription.confirmed?
+    assert_has_stream 'NotificationsChannel'
+  end
+
+  test 'unsubscribes from stream' do
+    subscribe
+
+    assert subscription.confirmed?
+    assert_has_stream 'NotificationsChannel'
+
+    unsubscribe
+    assert_no_streams
+  end
+
+  test 'broadcasts to the channel' do
+    subscribe
+
+    assert_broadcasts 'NotificationsChannel', 0
+    ActionCable.server.broadcast 'NotificationsChannel', { message: 'Hello, world!' }
+    assert_broadcasts 'NotificationsChannel', 1
+  end
 end
