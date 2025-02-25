fbfc1b77dc2cd1ea8d0e2cc965cf11cb72b293a0
Adenhall <jamesadenhall@gmail.com>
2025-02-25 15:57:38 +0700

    feat: add sentry for errors reporting

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'rack-cors'
 # For background jobs
 gem 'redis', '~> 5.2'
 
+gem 'sentry-rails'
+gem 'sentry-ruby'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = 'https://fb13107b3adba42629c401d0d32a9a2a@o4508878958297088.ingest.us.sentry.io/4508878958559232'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+end
